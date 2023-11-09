f5a46cf9cd3260754e22ab4c1fbfc7b91c4aaf61
RSAdrian9 <adrian4rs@gmail.com>
2023-11-09 10:22:55 +0100

    Primer commit: Agregar proyecto de Angular

--- src/app/components/note/note.component.ts
+++ src/app/components/note/note.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { INote } from 'src/app/model/INote';
 
 @Component({
   selector: 'app-note',
   templateUrl: './note.component.html',
   styleUrls: ['./note.component.css']
 })
-export class NoteComponent {
+export class NoteComponent implements OnInit{
+  @Input('note') public note:INote = {
+    id:-1,
+    title:'',
+    description:''
+  };
+
+  @Output() editNote = new EventEmitter<INote>();
+  @Output() removeNote = new EventEmitter<INote>();
+
+  constructor(){
+  }
+
+  ngOnInit(): void {
+    
+  }
+
+  public editNoteFn(){
+    if(this.note.id==-1) return;
+    this.editNote.emit(this.note);
+  }
+
+  public removeNoteFn(){
+    if(this.note.id==-1) return;
+    this.removeNote.emit(this.note);
+  }
 
 }
